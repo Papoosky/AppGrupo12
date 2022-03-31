6b3d8afb369169fc6495df883a89121c86746c8e
Pablo Zuñiga <80722868+Papoosky@users.noreply.github.com>
2022-03-31 14:49:51 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 #include "header.h"
 #define MAXCHAR 1000
 
-#define READ_ONLY "r"
+
 
 void showContents(FILE *myFile);
+void showContentsAsStruct(FILE *pFile);
+
 FILE * openingFile(char *filename){
     FILE *fp;
-    fp = fopen(filename,READ_ONLY);
+    fp = fopen(filename,"r");
     return fp;
 }
 
@@ -19,18 +21,29 @@ int main(int argc, char *argv[] ){
 
     FILE *fp = openingFile(argv[1]);
 
-    showContents(fp);
-
+   // showContents(fp);
+    showContentsAsStruct(fp);
 
     return 0;
 
 }
+void showContentsAsStruct(FILE *pFile) {
+    Libro *libros;
+    //paso el contenido del file a un array de personas
+    libros = getLibros(pFile);
 
+    //accedo a la cantidad de registro que lei
+    for (int i = 0; i < registryCount; ++i) {
+        //puedo solo imprimir su mail por ejemplo
+        printf("%s \n", libros[i].autor);
+    }
+
+}
 void showContents(FILE *myFile) {
     char row[MAXCHAR];
     char *token;
     //muestra el contenido del archivo
-	//fgets(row, MAXCHAR, myFile);
+    fgets(row, MAXCHAR, myFile);
     while (!feof(myFile)){
         //obtiene la linea siguiente
         fgets(row, MAXCHAR, myFile);
